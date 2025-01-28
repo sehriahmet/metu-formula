634138e06727dd33d53c2b1732f2dda7c8fbbac8
sehriahmet <sehriyarogluahmet@gmail.com>
2025-01-28 04:44:34 +0300

    initial commit

---
+++ navbar.js
@@ -0,0 +1,31 @@
+const NavbarSelector = 'nav#navbar';
+const navbar = document.querySelector(NavbarSelector);
+
+function addStyles (selector) {
+    
+    const styles = ` 
+        ${selector} {
+            transition: height 0.5s ease-in-out
+        }
+    
+        ${selector} .small {
+            height: 60px;
+        }
+    `;
+    
+    const styleTag = document.createElement('style');
+    styleTag.innerHTML = styles;
+    document.head.appendChild(styleTag);
+
+}
+
+addStyles(NavbarSelector);
+
+window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    if (window.scrollY > 10) {
+        navbar.classList.add('small');
+    } else {
+        navbar.classList.remove('small');
+    }
+});
---
+++ script.js
@@ -0,0 +1,8 @@
+function feature(e, featureClassName) {
+    var element = document.getElementsByClassName('tab-item');
+    for (var i = 0; i < element.length; i++) {
+      var shouldBeActive = element[i].classList.contains(featureClassName);
+      element[i].classList.toggle('active', shouldBeActive);
+    }
+  }
+  
